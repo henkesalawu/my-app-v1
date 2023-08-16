88fc7feb69133627f20f8a1734ce2a4ec6f1838f
henkesalawu <henkesalawu@gmail.com>
2023-08-16 18:38:26 +0100

    End of section 3

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
+import EventDashboard from '../../features/events/eventDashboard/EventDashboard';
+import NavBar from '../../features/nav/NavBar';
+import { Container } from 'semantic-ui-react';
 
 function App() {
+  const [formOpen, setFormOpen] = useState(false);
+
   return (
-    <div className="App">
+    <div>
+    <NavBar setFormOpen={setFormOpen} />
+    <Container className='main'>
+    <EventDashboard formOpen={formOpen} setFormOpen={setFormOpen}/>
+    </Container>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import '../app/layout/styles.css';
-import App from '../app/layout/App';
+import 'semantic-ui-css/semantic.min.css';
+import './app/layout/styles.css';
+import App from './app/layout/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
